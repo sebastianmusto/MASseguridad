3fe02a46e61b7af9aea10a6b4211256af9a635d7
valevillaverde <valeria.villaverdearq@gmail.com>
2022-10-11 08:47:02 -0300

    home reducido

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-
+// MENU DESPLEGABLE
 const ham = document.querySelector('.ham');
 const enlaces = document.querySelector('.enlaces-menu');
 const barras = document.querySelectorAll('.ham span');
@@ -11,3 +11,50 @@ ham.addEventListener('click', () => {
     barras.forEach(child => {child.classList.toggle('animado')});
     ham.classList.toggle('girar');
 });
+
+
+//CARROUSEL
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  } 
+ 
+// automatic carousel 
+// let slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 5000); // Change image every 5 seconds
+// } 
