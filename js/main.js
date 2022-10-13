a0d797e12953ff9a7689c7c487eea563ebbef02a
valevillaverde <valeria.villaverdearq@gmail.com>
2022-10-13 18:45:55 -0300

    cambios css

--- js/main.js
+++ js/main.js
@@ -7,14 +7,13 @@ document.getElementById("idheader").innerHTML = `
 </div>
 
 <ul class="enlaces-menu">
-    <li><a href="#">Home</a></li>
-    <li><a href="#">Preguntas Frecuentes</a></li>
-    <li><a href="#">Consejos</a></li>
+    <li><a href="/QueHacer.html">Preguntas Frecuentes</a></li>
+    <li><a href="/consejos.html">Consejos</a></li>
     <li><a href="https://www.facebook.com" target="_blank"><i class="fa-brands fa-facebook-f"></i></a></li>
     <li><a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a></li>
     <li><a href="https://www.youtube.com" target="_blank"><i class="fab fa-youtube"></i></a></li>
-    <li><a class="boton-principal href="FormularioCAC">Contactanos!</a></li>
-</ul>
+    <li><a class="boton-principal" href="Contacto.html">Contactanos!</a></li>
+    </ul>
 
 <button class="ham" type="button">   
     <span class="br-1"></span>
@@ -23,58 +22,39 @@ document.getElementById("idheader").innerHTML = `
 </button>                       
 </nav>
 `
-//FOOTER 1 > idfooter
+
+//FOOTER  > idfooter
 
 document.getElementById("idfooter").innerHTML = `
-<div class="contenidofooter">
-<div class="footer-informacion">
-    <h3>Sobre <span>MÁS</span>seguridad</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, illum.</p>
-</div>
-
-<div class="menu">
-    <h3>Redes <br> <span>Sociales</span></h3>
-    <nav class="redes-sociales">
-        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-        <a href="#"><i class="fab fa-instagram"></i></a>
-        <a href="#"><i class="fab fa-youtube"></i></a>
-    </nav>
-</div>
-<!-- menu -->
-</div>
-<p class="copyright"><span>MÁS</span>seguridad2022 | by <a href="">CaC2022</a></p>
-`
-
-//FOOTER 2 > idfooter2
-
-document.getElementById("idfooter2").innerHTML = `
-<div class="grupo-1">
-<div class="box">
-    <figure>
-        <a href="#">
-            <img src="logo.JPG" alt="Logo AntiHacking">
-        </a>
-    </figure>
-</div>
-<div class="box">
-    <h2>Sobre nosotros</h2>
-    <p>Somos un grupo de estudiantes del programa codo a codo, que desarrollaron esta pagina para
-        concientizar sobre cyberseguridad.</p>
-
-</div>
-<div class="box">
-    <h2>Seguinos</h2>
-    <div class="red-social">
-        <a href="#" class="fa fa-facebook" aria-hidden="true"></a>
-        <a href="#" class="fa fa-instagram" aria-hidden="true"></a>
-        <a href="#" class="fa fa-twitter" aria-hidden="true"></a>
-        <a href="#" class="fa fa-youtube" aria-hidden="true"></a>
+<div class="pie-pagina">
+    <div class="grupo-1">
+        <div class="box">
+            <figure>
+                <div class="logo">
+                    <a href="index.html"><p><i class="fa-solid fa-unlock-keyhole"></i> <span>MÁS</span> seguridad</p></a>
+                </div>
+            </figure>
+        </div>
+        <div class="box">
+            <h2>Sobre nosotros</h2>
+            <p>Somos un grupo de estudiantes del programa codo a codo, que desarrollaron esta pagina para concientizar sobre cyberseguridad.</p>
+            
+        </div>
+        <div class="box">
+            <h2>Seguinos</h2>
+            <div class="red-social">
+                <a href="https://www.facebook.com" class="fa fa-facebook" aria-hidden="true"></a>
+                <a href="https://www.instagram.com" class="fa fa-instagram" aria-hidden="true"></a>
+                <a href="https://www.twitter.com" class="fa fa-twitter" aria-hidden="true"></a>
+                <a href="https://www.youtube.com" class="fa fa-youtube" aria-hidden="true"></a>
+            </div>
+        </div>
+    </div>
+    <div class="grupo-2">
+        <small>© 2022 <b>Mas seguridad </b>Todos los Derechos Reservados.</small>
     </div>
 </div>
-</div>
-<div class="grupo-2">
-<small>© 2022 <b>Mas seguridad </b>Todos los Derechos Reservados.</small>
-</div>
+
 `
 
 // MENU DESPLEGABLE
