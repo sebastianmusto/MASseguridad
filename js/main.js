b1d662fc537327ca92a66e489c76ed064da026fe
sebastianmusto <sebastianmusto@gmail.com>
2022-10-20 22:47:44 -0300

    Se agregue videos en consejos.html

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ document.getElementById("idfooter").innerHTML = `
         </div>
         <div class="box">
             <h2>Sobre nosotros</h2>
-            <p>Somos un grupo de estudiantes del programa codo a codo, que desarrollaron esta pagina para concientizar sobre cyberseguridad.</p>
+            <p>Somos un grupo de estudiantes del programa codo a codo, que desarrollaron esta pagina para concientizar sobre ciberseguridad.</p>
             
         </div>
         <div class="box">
@@ -63,9 +63,9 @@ const barras = document.querySelectorAll('.ham span');
 
 
 ham.addEventListener('click', () => {
-    enlaces.classList.toggle('activado');
-    barras.forEach(child => {child.classList.toggle('animado')});
-    ham.classList.toggle('girar');
+  enlaces.classList.toggle('activado');
+  barras.forEach(child => { child.classList.toggle('animado') });
+  ham.classList.toggle('girar');
 });
 
 
@@ -75,68 +75,68 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
+  showSlides(slideIndex += n);
+}
+
 // Thumbnail image controls
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  
-  function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-  } 
-  
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
 // Validación de Formulario
 
-function validarEnviar(){
-    if(document.fvalida.nombre.value.length ==0){
-      alert("Por favor escriba su nombre")
-      document.fvalida.nombre.focus()
-      return 0;
-    }
-    if(document.fvalida.apellido.value.length ==0){
-      alert("Por favor escriba su apellido")
-      document.fvalida.apellido.focus()
-      return 0;
-    }
-    if(document.fvalida.email.value.length ==0){
-      alert("Por favor escriba su correo")
-      document.fvalida.email.focus()
-      return 0;
-    }
-    if(document.fvalida.telefono.value.length ==0){
-      alert("Por favor escriba un telefono")
-      document.fvalida.telefono.focus()
-      return 0;
-    }
-    if(document.fvalida.nombre.value.length ==0){
-      alert("Por favor escriba un nombre")
-      document.fvalida.nombre.focus()
-      return 0;
-    }
-    if(document.fvalida.mensaje.value.length ==0){
-      alert("Por favor dejanos tu mensaje")
-      document.fvalida.mensaje.focus()
-      return 0;
-    }
-    alert("Muchas gracias por contactarte con nosotros.Te responderemos a la brevedad")
-    document.fvalida.submit()
+function validarEnviar() {
+  if (document.fvalida.nombre.value.length == 0) {
+    alert("Por favor escriba su nombre")
+    document.fvalida.nombre.focus()
+    return 0;
   }
+  if (document.fvalida.apellido.value.length == 0) {
+    alert("Por favor escriba su apellido")
+    document.fvalida.apellido.focus()
+    return 0;
+  }
+  if (document.fvalida.email.value.length == 0) {
+    alert("Por favor escriba su correo")
+    document.fvalida.email.focus()
+    return 0;
+  }
+  if (document.fvalida.telefono.value.length == 0) {
+    alert("Por favor escriba un telefono")
+    document.fvalida.telefono.focus()
+    return 0;
+  }
+  if (document.fvalida.nombre.value.length == 0) {
+    alert("Por favor escriba un nombre")
+    document.fvalida.nombre.focus()
+    return 0;
+  }
+  if (document.fvalida.mensaje.value.length == 0) {
+    alert("Por favor dejanos tu mensaje")
+    document.fvalida.mensaje.focus()
+    return 0;
+  }
+  alert("Muchas gracias por contactarte con nosotros.Te responderemos a la brevedad")
+  document.fvalida.submit()
+}
 
-// automatic carousel 
+// automatic carousel
 // let slideIndex = 0;
 // showSlides();
 
